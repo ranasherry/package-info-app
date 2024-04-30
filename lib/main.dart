6e00ab14eed1388bb174520956393e2e84b91176
ranasherry <ranasherry94@gmail.com>
2024-04-30 14:00:45 +0500

    meta added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:all_network_packages/firebase_options.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'app/modules/routes/app_pages.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     // DeviceOrientation.landscapeRight,
@@ -14,11 +20,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      navigatorObservers: <NavigatorObserver>[observer],
       theme: ThemeData(
         useMaterial3: true,
         appBarTheme: AppBarTheme(
